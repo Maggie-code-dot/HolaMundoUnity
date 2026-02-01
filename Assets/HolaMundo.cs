c11342ce93bf38bee073b17baae40b39f8cf68fc
Maggie-code-dot <garciaveladormaggie@gmail.com>
2026-01-31 20:27:45 -0600

    Subiendo código

---
+++ Assets/HolaMundo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Este mensaje aparecerá al iniciar el juego 
+        Debug.Log("Hola Mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
